ccc1b6e831db25d68f10377eed41f8fe42925fd8
oussama goumghar <ogoumghar@sqli.com>
2025-06-02 14:23:57 +0100

    Fix 1 issues in app/(home)/loading.tsx

--- app/(home)/loading.tsx
+++ app/(home)/loading.tsx
@@ -8,7 +8,7 @@ export default function Loading() {
       <div className="flex flex-wrap">
         {Array.from({ length: 20 }).map((_, i) => (
           <div
-            key={i}
+            key={`skeleton-${i}`}
             className="basis-1/2 rounded-md pb-4 pr-4 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
           >
             <Skeleton className="aspect-poster w-full" />
