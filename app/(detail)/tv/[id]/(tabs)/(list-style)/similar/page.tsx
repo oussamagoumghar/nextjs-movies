ef7af959bdd147784a5602529df76005b36ebde4
oussama goumghar <ogoumghar@sqli.com>
2025-06-02 14:21:46 +0100

    Fix 1 issues in app/(detail)/tv/[id]/(tabs)/(list-style)/similar/page.tsx

--- app/(detail)/tv/[id]/(tabs)/(list-style)/similar/page.tsx
+++ app/(detail)/tv/[id]/(tabs)/(list-style)/similar/page.tsx
@@ -19,7 +19,7 @@ export const metadata = {
 export default async function DetailSimilar({
   params,
   searchParams,
-}: DetailSimilarProps) {
+}: Readonly<DetailSimilarProps>) {
   const {
     results: tvShows,
     total_pages: totalPages,
