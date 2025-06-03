88001cc7611c8f8ebcfc59cb1b24ad88e971a888
oussama goumghar <ogoumghar@sqli.com>
2025-06-03 17:20:45 +0100

    Fix 1 issues in app/(lists)/movie/popular/page.tsx

--- app/(lists)/movie/popular/page.tsx
+++ app/(lists)/movie/popular/page.tsx
@@ -3,7 +3,7 @@ import { pages } from "@/config"
 import { MovieList } from "@/components/movie-list"
 
 interface ListPageProps {
-  searchParams?: Record<string, string>
+  readonly searchParams?: Record<string, string>
 }
 
 export async function generateMetadata() {
