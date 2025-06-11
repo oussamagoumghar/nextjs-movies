d7519f2e7864cb8a1c13798a9179934c7fb09414
oussama goumghar <ogoumghar@sqli.com>
2025-06-11 15:27:07 +0100

    Fix 1 issues in app/(detail)/movie/[id]/(tabs)/videos/loading.tsx

--- app/(detail)/movie/[id]/(tabs)/videos/loading.tsx
+++ app/(detail)/movie/[id]/(tabs)/videos/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className="grid gap-4 md:grid-cols-2 xl:grid-cols-3">
       {Array.from({ length: 20 }).map((_, i) => (
-        <Skeleton key={i} className="aspect-video w-full rounded-md" />
+        <Skeleton key={`skeleton-${i}`} className="aspect-video w-full rounded-md" />
       ))}
     </div>
   )
