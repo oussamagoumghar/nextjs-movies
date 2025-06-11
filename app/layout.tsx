81f42c76a4f64b0e02a3ff95d3f0c780193dbd87
oussama goumghar <ogoumghar@sqli.com>
2025-06-11 15:27:20 +0100

    Fix 2 issues in app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ interface RootLayoutProps {
   children: React.ReactNode
 }
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
@@ -45,7 +45,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <ThemeProvider attribute="class" defaultTheme="dark">
           <div
             className="relative flex min-h-screen flex-col bg-background"
-            vaul-drawer-wrapper=""
+            
           >
             <GridBg />
             <SiteHeader />
