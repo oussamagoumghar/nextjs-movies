937db2a7a9032b7d39b40a22dcd5423b4ba9702b
oussama goumghar <ogoumghar@sqli.com>
2025-06-11 15:27:07 +0100

    Fix 1 issues in app/(detail)/tv/[id]/(tabs)/(list-style)/loading.tsx

--- app/(detail)/tv/[id]/(tabs)/(list-style)/loading.tsx
+++ app/(detail)/tv/[id]/(tabs)/(list-style)/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className="grid-list">
       {Array.from({ length: 20 }).map((_, i) => (
-        <Skeleton key={i} className="aspect-poster w-full rounded-md" />
+        <Skeleton key={`skeleton-${i}`} className="aspect-poster w-full rounded-md" />
       ))}
     </div>
   )
